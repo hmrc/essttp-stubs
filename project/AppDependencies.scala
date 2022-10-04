dbff5d5ee532b0d681945431e201d1cdef936b76
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2022-10-04 10:04:40 +0100

    [JB][SUPP-559] make frequency 'Monthly' again...

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  val boostrapVersion = "7.4.0"
+  val boostrapVersion = "7.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.8.0",
     "com.beachape"      %% "enumeratum"                  % "1.7.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"          % "0.73.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.60.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.62.0"
   )
 
   val test: Seq[ModuleID] = Seq(
