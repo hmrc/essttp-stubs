ccc244a295c41da19cd34051dd360bc5ff3e18db
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2022-11-08 14:46:45 +0000

    [JB][SUPP-593] Amend arrangement/enact API models for VAT

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  val boostrapVersion = "7.8.0"
+  val boostrapVersion = "7.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.8.0",
     "com.beachape"      %% "enumeratum"                  % "1.7.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"          % "0.73.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.68.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.77.0"
   )
 
   val test: Seq[ModuleID] = Seq(
