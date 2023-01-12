010ab3ce4c9643d5fd4158b568b5e2592c010ec5
Jake Barnes <33868990+barnesjake@users.noreply.github.com>
2023-01-12 14:00:42 +0000

    Jake/supp 689 (#47)
    * Merge branch 'jake/SUPP-689' of /Users/jakebarnes/workspace/essttp-stubs with conflicts.
    
    * [JB][SUPP-689]
    - bump up cor version
    - sbt 1.7.2
    - enumeratum and scalatest minor version increases

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,13 +9,13 @@ object AppDependencies {
     "org.typelevel"     %% "cats-core"                   % "2.9.0",
     "com.beachape"      %% "enumeratum"                  % "1.7.2",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"          % "0.74.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.94.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.100.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % boostrapVersion,
     "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2",
-    "org.scalatest"          %% "scalatest"              % "3.2.14" ,
+    "org.scalatest"          %% "scalatest"              % "3.2.15" ,
     "org.pegdown"            %  "pegdown"                % "1.6.0" ,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"
   ).map(_ % Test)
