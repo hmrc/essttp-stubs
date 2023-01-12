d5e4a6adf72ade40bb1674168e688f3b39868473
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2023-01-12 12:10:03 +0000

    [JB][SUPP-689] - bump up cor version - sbt 1.7.2 - enumeratum and scalatest minor version increases

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,15 +7,15 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.9.0",
-    "com.beachape"      %% "enumeratum"                  % "1.7.0",
+    "com.beachape"      %% "enumeratum"                  % "1.7.2",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"          % "0.74.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.88.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.100.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % boostrapVersion,
     "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2",
-    "org.scalatest"          %% "scalatest"              % "3.2.14" ,
+    "org.scalatest"          %% "scalatest"              % "3.2.15" ,
     "org.pegdown"            %  "pegdown"                % "1.6.0" ,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"
   ).map(_ % Test)
