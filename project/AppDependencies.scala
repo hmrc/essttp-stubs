456bebb0e85a17b1ae907f58b1040cfea79845d2
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2023-10-10 15:45:32 +0100

    [KN][OPS-10905] Dependencies update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % boostrapVersion,
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2",
+    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.64.6",
     "org.scalatest"          %% "scalatest"              % "3.2.17" ,
     "org.pegdown"            %  "pegdown"                % "1.6.0" ,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"
