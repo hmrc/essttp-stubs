50b7f83aced509c12ec5d21695288f3ddeece25f
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2023-10-11 16:47:15 +0100

    [KN][OPS-10905] With new cor build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.10.0",
     "com.beachape"      %% "enumeratum"                  % "1.7.3",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.115.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.116.0"
   // format: ON
   )
 
