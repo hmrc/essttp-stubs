f70de20223ea04ed6556af919b2c2b04adc74518
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2024-01-03 09:39:42 +0000

    [AC][OPS-11446] play 3 upgrade (#66)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,24 +2,22 @@ import sbt._
 
 object AppDependencies {
 
-  val boostrapVersion = "8.0.0"
+  val boostrapVersion = "8.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"   % boostrapVersion,
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.10.0",
     "com.beachape"      %% "enumeratum"                  % "1.7.3",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.122.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.124.0"
   // format: ON
   )
 
   val test: Seq[ModuleID] = Seq(
     // format: OFF
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % boostrapVersion,
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.64.6",
-    "org.scalatest"          %% "scalatest"              % "3.2.17" ,
-    "org.pegdown"            %  "pegdown"                % "1.6.0" ,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % boostrapVersion,
+    "org.scalatest"          %% "scalatest"              % "3.2.17",
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.1"
   // format: ON
   ).map(_ % Test)
 }
