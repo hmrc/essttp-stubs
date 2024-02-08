d763763944f35aefea932a1d461a0aa9ae63cece
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2024-02-08 13:25:13 +0000

    [AC][OPS-11759] allow for SA in enactArrangement (#70)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,14 +9,14 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.10.0",
     "com.beachape"      %% "enumeratum"                  % "1.7.3",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.127.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "1.128.0"
   // format: ON
   )
 
   val test: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % boostrapVersion,
-    "org.scalatest"          %% "scalatest"              % "3.2.17",
+    "org.scalatest"          %% "scalatest"              % "3.2.18",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.1"
   // format: ON
   ).map(_ % Test)
