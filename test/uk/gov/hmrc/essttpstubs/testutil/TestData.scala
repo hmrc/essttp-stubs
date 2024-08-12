8e995c31e1611fede8af8c6675286601af96529d
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2024-08-12 10:39:24 +0100

    [KN][OPS-12424] Fix test

--- test/uk/gov/hmrc/essttpstubs/testutil/TestData.scala
+++ test/uk/gov/hmrc/essttpstubs/testutil/TestData.scala
@@ -54,7 +54,7 @@ object TestData {
           )
         ),
         customerPostcodes               = List(
-          CustomerPostcode(addressPostcode = Postcode(SensitiveString("test-postcode")), postcodeDate = PostcodeDate("2022-01-01"))
+          CustomerPostcode(addressPostcode = Postcode(SensitiveString("test-postcode")), postcodeDate = PostcodeDate(LocalDate.of(2022, 1, 1)))
         ),
         regimePaymentFrequency          = PaymentPlanFrequencies.Monthly,
         paymentPlanFrequency            = PaymentPlanFrequencies.Monthly,
