02d2ebf4290aaa17de6ca9f0d41a4ed032a2a077
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2025-06-06 10:03:47 +0100

    [KN][OPS-13669] Add 3 new fields to Enact API - SA changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "9.11.0"
+  val boostrapVersion = "9.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.13.0",
-    "com.beachape"      %% "enumeratum"                  % "1.7.6",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "2.6.0"
+    "com.beachape"      %% "enumeratum"                  % "1.9.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "2.7.0",
   // format: ON
   )
 
