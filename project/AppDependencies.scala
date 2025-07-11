ccd75992c5297e7eaefa3b44b97925d41257845c
Kostas Ntanos <kostas@ntanos.co.uk>
2025-07-11 12:18:26 +0100

    [KN][OPS-13851] Backend dependency update (#120)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.13.0",
     "com.beachape"      %% "enumeratum"                  % "1.9.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "2.9.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "2.10.0",
   // format: ON
   )
 
