431b7b11c67115b68dc62f89a75a5121bbad7264
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2026-03-26 10:27:31 +0000

    [AC][OPS-14745] dependency updates (#124)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "10.5.0"
+  val boostrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"   % boostrapVersion,
     "org.typelevel"     %% "cats-core"                   % "2.13.0",
-    "com.beachape"      %% "enumeratum"                  % "1.9.4",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "2.18.0",
+    "com.beachape"      %% "enumeratum"                  % "1.9.6",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"  % "2.19.0",
   // format: ON
   )
 
   val test: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % boostrapVersion,
-    "org.scalatest"          %% "scalatest"              % "3.2.19",
+    "org.scalatest"          %% "scalatest"              % "3.2.20",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2"
   // format: ON
   ).map(_ % Test)
